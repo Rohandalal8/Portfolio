4fccf9ed0fa17e2bb7efbf1eef055e806b83641f
Rohandalal8 <rohandalal079@gmail.com>
2025-03-07 22:05:40 +0530

    Fix code

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const nodemailer = require('nodemailer');
 const app = express();
+require('dotenv').config();
 
 app.use(express.static("public"));
 app.use(bodyparser.urlencoded({ extended: true }));
@@ -16,14 +17,14 @@ app.post("/", (req, res) => {
     let transpoter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'rohandalal079@gmail.com',
-            pass: 'bqap vfmq vpfl cvto'
+            user: 'process.env.EMAIL_USER',
+            pass: 'process.env.EMAIL_PASS'
         }
     });
     let mailOptions = {
-        from: '"Rohan" <rohandalal079@gmail.com>',
+        from: '"Rohan" <process.env.EMAIL_USER>',
         to: req.body.email,
-        cc: 'rohandalal079@gmail.com',
+        cc: 'process.env.EMAIL_USER',
         subject: 'Thanks for for giving feedback ' + name,
         text: 'Thanks for your message You have sent to us --> ' + message
     }
